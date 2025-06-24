283421dc8d40a80ba569f201ddde72c1ae740640
mannfiqq <mannnsyafiq@gmail.com>
2025-06-24 18:22:51 +0800

    Nav Menu Frames and Link

---
+++ zgallery/gallery.js
@@ -0,0 +1,74 @@
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('switch')
+const darkTheme = 'dark-theme'
+const iconTheme = '.sun'
+
+//previously selected
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//obtaining the current theme
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? '.moon' : '.sun'
+
+//validate the previously chosen
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === '.moon' ? 'add' : 'remove'](iconTheme)
+}
+
+//activate and deactivate theme using button
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu Show */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu Hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+    //when clicked, show-menu will be removed as class
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+    const header = document.getElementById('header')
+    //add a class if bottom offset is greater than 50viewport
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
+/*=============== SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    //when scroll is higher than 350vh, add show-scroll class
+    this.scrollY >= 350 ? scrollUP.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
